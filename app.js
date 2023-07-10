cb6aa6cea18239947d0996db430ee73da6cc44ab
Lanways <mcbluedd@gmail.com>
2023-07-10 23:16:04 +0800

    feat: add pgsql

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const express = require('express')
+const { Pool } = require('pg')
+
 const app = express()
+
+const pool = new Pool({
+  host: 'localhost',
+  port: 5432,
+  database: 'trade_tracker',
+  user: 'postgres',
+  password: 'password'
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Failed to connect to the PostgreSQL database:', err);
+  } else {
+    console.log('Connected to the PostgreSQL database successfully!');
+  }
+  pool.end();
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!omg')
