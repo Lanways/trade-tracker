0b714660c1b95d3e29c5323dbb204392602eb9ad
Lanways <mcbluedd@gmail.com>
2023-07-15 17:25:02 +0800

    feat: modify transaction seed

--- seeds/002-transaction-seed-file.js
+++ seeds/002-transaction-seed-file.js
@@ -18,28 +18,28 @@ if (process.env.NODE_ENV === "production") {
   })
 }
 
-(async () => {
+async function seedTransaction() {
   const res = await pool.query('SELECT id FROM users')
-
-  for (let userId of res.rows.map(row => row.id)) {
-    try {
-      for (let i = 0; i < 10; i++) {
-
-        const price = Math.floor(Math.random() * 1000) + 17000
-        const quantity = Math.floor(Math.random() * 9) + 1
-
-        await pool.query(
-          `INSERT INTO transactions (user_id, action, quantity, price, transaction_date, description, created_on, updated_on)
+  const countRes = await pool.query('SELECT COUNT(*) AS count FROM users')
+  const count = countRes.rows[0].count
+  if (count === 0) {
+    for (let userId of res.rows.map(row => row.id)) {
+      try {
+        for (let i = 0; i < 10; i++) {
+          const price = Math.floor(Math.random() * 1000) + 17000
+          const quantity = Math.floor(Math.random() * 9) + 1
+          await pool.query(
+            `INSERT INTO transactions (user_id, action, quantity, price, transaction_date, description, created_on, updated_on)
         VALUES ($1, $2, $3, $4, CURRENT_TIMESTAMP, $5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)`,
-          [userId, i % 2 === 0 ? 'buy' : 'sell', quantity, price, `Description for transaction ${i}`]
-        )
+            [userId, i % 2 === 0 ? 'buy' : 'sell', quantity, price, `Description for transaction ${i}`]
+          )
+        }
+      } catch (err) {
+        console.error(`Error: ${err.message}`)
       }
-
-    } catch (err) {
-      console.error(`Error`)
     }
-
   }
-  console.log('success created seeds')
+  console.log('success created user seeds')
   await pool.end()
-})()
+}
+seedTransaction().catch(err => console.error(err))
