9d2c02d7124ff6c3cc304313a7ff84ae67509685
Lanways <mcbluedd@gmail.com>
2023-07-20 13:51:26 +0800

    feat: add create closures table sql file

--- migrations/202307181630-007-create-closures-table.sql
+++ migrations/202307181630-007-create-closures-table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS closures (
   closure_id SERIAL PRIMARY KEY,
-  transaction_id INT REFERENCES transactions(id),
-  quantity INT NOT NULL,
+  open_transaction_id INT REFERENCES transactions(id),
+  closed_transaction_id INT REFERENCES transactions(id),
+  closed_quantity INT NOT NULL,
   price DECIMAL(10, 2) NOT NULL,
-  closed_on TIMESTAMP NOT NULL,
   created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
