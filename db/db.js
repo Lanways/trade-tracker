3afd79df7964ab94e80211cfadcf6e44ff9669d9
JimmyLin <59187640+deamo771003@users.noreply.github.com>
2023-07-20 17:03:14 +0800

    Update db.js

--- db/db.js
+++ db/db.js
@@ -62,19 +62,15 @@ module.exports = {
     return res.rows
   },
   changePublic: async (newPublicValue, transactionId) => {
-    const query = 'UPDATE transactions SET is_public = $1 WHERE id = $2'
-    const values = [newPublicValue, transactionId]
-    const res = await pool.query(query, values)
+    const res = await pool.query('UPDATE transactions SET is_public = $1 WHERE id = $2', [newPublicValue, transactionId])
     return res.rows[0]
   },
   deletePublic: async (newPublicValue, transactionId) => {
-    const query = 'UPDATE transactions SET is_public = $1 WHERE id = $2'
-    const values = [newPublicValue, transactionId]
-    const res = await pool.query(query, values)
+    const res = await pool.query('UPDATE transactions SET is_public = $1 WHERE id = $2', [newPublicValue, transactionId])
     return res.rows[0]
   },
   getPublicTransactions: async (values) => {
-    const res = await pool.query('SELECT * FROM transactions WHERE is_public = $1', [values])
+    const res = await pool.query('SELECT * FROM transactions WHERE is_public = true')
     return res.rows
   }
 }
