760a9dc5b20221dffff3c86a0960086318c21627
JimmyLin <disillusioned2077@gmail.com>
2023-07-29 14:36:00 +0800

    feat: modify/router-and-refactor-replies-seed

--- seeds/004-replies-seed-file.js
+++ seeds/004-replies-seed-file.js
@@ -10,11 +10,10 @@ module.exports = async function seedReplies(pool) {
     // 隨機 20 個 user_id 對隨機 50 個 transactions_id add content 加入 replies
     for (let userId of resUsersId.rows.map(us => us.id)) {
       let resTransactionId = await pool.query(`SELECT id FROM transactions ORDER BY RANDOM() LIMIT 50`)
-      const TransactionsId = resTransactionId.rows.map(row => row.id)
       for (let i = 0; i < 50; i++) {
         const content = faker.lorem.sentence()
         try {
-          await db.postReply(userId, TransactionsId[i], content)
+          await db.postReply(userId, resTransactionId.rows[i].id, content)
         } catch (err) {
           console.log(err)
         }
