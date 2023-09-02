c9f8d8977b6899ef120b73fe20829478a4e5e83c
Lanways <mcbluedd@gmail.com>
2023-09-02 14:50:19 +0800

    refactor: add credentials to cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000
 
 const corsOptions = {
   origin: ['https://owenlu0125.github.io', 'http://localhost:5501'],
-  optionsSuccessStatus: 200
+  credentials: true,
 }
 const cookieParser = require('cookie-parser')
 
